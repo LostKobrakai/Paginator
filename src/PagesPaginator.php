a32521760799a4e25d09afaf5b45b8d43c4510e1
Benjamin Milde <benni@kobrakai.de>
2016-07-02 14:21:15 +0200

    Added docblocks

--- src/PagesPaginator.php
+++ src/PagesPaginator.php
@@ -1,6 +1,16 @@
 <?php
 
+/**
+ * Class PagesPaginator
+ */
 class PagesPaginator extends Paginator{
+
+	/**
+	 * @param int $start
+	 * @param int $limit
+	 * @param int $total
+	 * @return PageArray
+	 */
 	protected function getStorage($start, $limit, $total)
 	{
 		return (new PageArray())
@@ -9,20 +19,41 @@ class PagesPaginator extends Paginator{
 			->setTotal($total);
 	}
 
+	/**
+	 * @param array $selectors
+	 * @return int
+	 */
 	protected function getTotal($selectors){
 		// Does currently not support selectors with or-groups in it
 		return wire('pages')->count('(id=[' . implode(']), (id=[', $selectors) . '])');
 	}
 
+	/**
+	 * @param string $selector
+	 * @return int
+	 */
 	protected function getNumberOfItemsForSelector($selector){
 		return wire('pages')->count($selector);
 	}
 
+	/**
+	 * @param string $selector
+	 * @param int   $key
+	 * @param int   $start
+	 * @param int   $limit
+	 * @param PageArray $storage
+	 * @return PageArray
+	 */
 	protected function getItems($selector, $key, $start, $limit, $storage)
 	{
 		return wire('pages')->find($selector . ", start=$start, limit=$limit");
 	}
 
+	/**
+	 * @param PageArray $storage
+	 * @param PageArray $items
+	 * @return PageArray
+	 */
 	protected function addToStorage($storage, $items)
 	{
 		// Keep set total untouched
--- src/Paginator.php
+++ src/Paginator.php
@@ -1,6 +1,18 @@
 <?php
 
+/**
+ * Class Paginator
+ */
 abstract class Paginator{
+
+	/**
+	 * Supply an array of selectors and get back paginated items from the concatinated results of all the selectors
+	 *
+	 * @param array $selectors Some kind of selectors as array
+	 * @param int $pageNum Current page starting on 1
+	 * @param int $limit The number of items of each page
+	 * @return mixed $storage
+	 */
 	public function __invoke($selectors, $pageNum, $limit){
 		$pageNum -= 1; // Zero-based
 		$total = $this->getTotal($selectors);
@@ -29,13 +41,51 @@ abstract class Paginator{
 		return $storage;
 	}
 
-	abstract protected function getStorage($start, $limit, $total);
-
+	/**
+	 * Retrieve the total number of items for the supplied selectors
+	 *
+	 * @param array $selectors
+	 * @return int
+	 */
 	abstract protected function getTotal($selectors);
 
+	/**
+	 * Return the intended storage type to be used.
+	 *
+	 * @param int $start The item to start with for the current page
+	 * @param int $limit The limit
+	 * @param int $total The total number of items available from selectors
+	 * @return mixed
+	 */
+	abstract protected function getStorage($start, $limit, $total);
+
+	/**
+	 * Return the number of items for the given selector
+	 *
+	 * @param mixed $selector
+	 * @return int
+	 */
 	abstract protected function getNumberOfItemsForSelector($selector);
 
+	/**
+	 * Get the items for the current selector according to the start and limit.
+	 * The current selectors index as well as the storage obj might be handy.
+	 *
+	 * @param mixed $selector
+	 * @param int $key
+	 * @param int $start
+	 * @param int $limit
+	 * @param mixed $storage
+	 * @return mixed
+	 */
 	abstract protected function getItems($selector, $key, $start, $limit, $storage);
 
+	/**
+	 * Add the found items to the storage and return it
+	 *
+	 * @param mixed $storage
+	 * @param mixed$items
+	 * @return mixed
+	 */
 	abstract protected function addToStorage($storage, $items);
 }
